0cc8e4f1c27ada8045d46c236f7593a9596d7826
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-04-07 17:40:49 +0200

    destrcture recipe on RecipeCard props

--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -5,25 +5,27 @@ interface RecipeCardProps {
   recipe: Recipe;
 }
 
-const RecipeCard = ({ recipe }: RecipeCardProps): JSX.Element => {
+const RecipeCard = ({
+  recipe: { name, categoryName, duration, complexity, people, _id, photo },
+}: RecipeCardProps): JSX.Element => {
   return (
     <div className="recipe">
       <img
         className="recipe-image"
-        src={recipe.photo ?? "http://via.placeholder.com/640x360"}
-        alt={recipe.name}
+        src={photo ?? "http://via.placeholder.com/640x360"}
+        alt={name}
       />
       <div className="recipe-info">
         <div>
-          <p className="recipe-category">{recipe.categoryName}</p>
-          <h1 className="recipe-name">{recipe.name}</h1>
+          <p className="recipe-category">{categoryName}</p>
+          <h1 className="recipe-name">{name}</h1>
           <div className="recipe-stuff">
-            <p className="recipe-time">{recipe.duration} minutes</p>
-            <p className="recipe-complexity">{recipe.complexity}</p>
-            <p className="recipe-people">{recipe.people} people</p>
+            <p className="recipe-time">{duration} minutes</p>
+            <p className="recipe-complexity">{complexity}</p>
+            <p className="recipe-people">{people} people</p>
           </div>
         </div>
-        <Link className="recipe-button" to="/detail" key={recipe._id}>
+        <Link className="recipe-button" to="/detail" key={_id}>
           See more
         </Link>
       </div>
