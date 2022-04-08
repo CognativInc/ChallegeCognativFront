5825f77e444589729972d891fe4b4f7be3a1663c
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-04-08 11:07:02 +0200

    added styles with styled-components in Recommended component

--- src/components/Recommended/Recommended.tsx
+++ src/components/Recommended/Recommended.tsx
@@ -1,22 +1,52 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import fakeRecommendations from "../../data/fakeRecomendations";
+
+const RecommendedTitle = styled.h2`
+  font-size: 30px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 20px;
+`;
+
+const RecomendationsContainer = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  height: 200px;
+  align-items: stretch;
+
+  li {
+    list-style: none;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 20px;
+  }
+`;
 
 const Recommended = () => {
   return (
-    <div>
-      <h1 className="title">Recommended</h1>
-      <div className="recommendations">
+    <>
+      <RecommendedTitle>Recommended</RecommendedTitle>
+      <RecomendationsContainer>
         {fakeRecommendations.map((recomendation) => (
-          <Link to="/detail" key={recomendation._id}>
-            <img
-              className="recommended-image"
-              src={recomendation.photo ?? "http://via.placeholder.com/640x360"}
-              alt={recomendation.name}
-            />
-          </Link>
+          <li>
+            <Link to="/detail" key={recomendation._id}>
+              <img
+                src={
+                  recomendation.photo ?? "http://via.placeholder.com/640x360"
+                }
+                alt={recomendation.name}
+              />
+            </Link>
+          </li>
         ))}
-      </div>
-    </div>
+      </RecomendationsContainer>
+    </>
   );
 };
 
