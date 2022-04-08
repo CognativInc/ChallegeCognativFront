c3c1afabc84da4784f31e5b6149894434732f2ee
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-04-08 14:09:56 +0200

    added generic Header on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Detail from "./pages/recipe/Detail";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="detail" element={<Detail />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="detail" element={<Detail />} />
+      </Routes>
+    </>
   );
 }
 
