d851cda56c5d263d21ac60bd7cb0a048b17c8179
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-04-09 19:02:49 +0200

    added styles to Detail page with styled components

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,8 +1,87 @@
 import { useParams } from "react-router-dom";
-import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import styled from "styled-components";
 import fakeRecipes from "../../data/fakeRecipes";
 import { Recipe } from "../../Interfaces/RecipeInterface";
 import "./style.css";
+
+const DetailContainer = styled.section`
+  background-color: #fafafa;
+  padding-top: 50px;
+`;
+
+const TopInfoContainer = styled.div`
+  display: flex;
+  padding: 20px 100px;
+
+  img {
+    height: 400px;
+    border-radius: 20px;
+  }
+
+  & > div:nth-child(1n) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex: 1;
+
+    & > p:nth-child(1n) {
+      font-size: 20px;
+      font-weight: bold;
+      color: #666;
+    }
+
+    h2 {
+      font-size: 25px;
+      font-weight: bold;
+      color: #333;
+      margin-top: 10px;
+    }
+
+    div:nth-child(3) {
+      display: flex;
+      margin-top: 10px;
+      gap: 30px;
+
+      p {
+        font-size: 18px;
+        color: #666;
+      }
+    }
+
+    div:nth-child(4) {
+      margin-top: 40px;
+
+      h4 {
+        font-size: 20px;
+        color: #333;
+        font-weight: bold;
+      }
+
+      p {
+        margin-top: 10px;
+        text-align: center;
+      }
+    }
+  }
+`;
+
+const BottomContainer = styled.div`
+  padding: 20px 100px;
+
+  h4 {
+    font-size: 30px;
+    font-weight: bold;
+    color: #333;
+    margin-bottom: 20px;
+  }
+
+  p {
+    font-size: 18px;
+    color: #666;
+    line-height: 1.5;
+    letter-spacing: 0.2px;
+  }
+`;
 
 const Detail = (): JSX.Element => {
   const { id } = useParams();
@@ -12,38 +91,33 @@ const Detail = (): JSX.Element => {
     <>
       {recipe ? (
         <>
-          <SectionTitle text="Food recipes" />
-          <div className="page">
-            <div className="top">
+          <DetailContainer>
+            <TopInfoContainer>
               <img
                 src={recipe.photo ?? "http://via.placeholder.com/640x360"}
                 alt={recipe.name}
               />
-              <div className="top-information">
-                <p className="recipe-detail-category">{recipe.categoryName}</p>
-                <h1 className="recipe-detail-name">{recipe.name}</h1>
-                <div className="recipe-detail-stuff">
-                  <p className="recipe-detail-time">
-                    {recipe.duration} minutes
-                  </p>
-                  <p className="recipe-detail-complexity">
-                    {recipe.complexity}
-                  </p>
-                  <p className="recipe-detail-people">{recipe.people} people</p>
+              <div>
+                <p>{recipe.categoryName}</p>
+                <h2>{recipe.name}</h2>
+                <div>
+                  <p>{recipe.duration} minutes</p>
+                  <p>{recipe.complexity}</p>
+                  <p>{recipe.people} people</p>
                 </div>
-                <div className="ingredients">
+                <div>
                   <h4>Ingredients</h4>
                   {recipe.ingredients.split("\r\n").map((ingredient) => (
                     <p key={ingredient}>{ingredient}</p>
                   ))}
                 </div>
               </div>
-            </div>
-            <div className="bottom">
-              <h4 className="description">Info</h4>
-              <p className="description-text">{recipe.description}</p>
-            </div>
-          </div>
+            </TopInfoContainer>
+            <BottomContainer>
+              <h4>Info</h4>
+              <p>{recipe.description}</p>
+            </BottomContainer>
+          </DetailContainer>
         </>
       ) : (
         <></>
