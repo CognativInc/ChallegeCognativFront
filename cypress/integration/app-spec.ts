e8a32775ef9b5bb87087871970dba3884c2693a8
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-04-11 00:59:14 +0200

    added recommended click test wit cyrpess

--- cypress/integration/app-spec.ts
+++ cypress/integration/app-spec.ts
@@ -1,15 +1,15 @@
 // type definitions for Cypress object "cy"
 /// <reference types="cypress" />
 
-describe("App", function () {
-  beforeEach(function () {
-    cy.visit("/login");
+describe("App", () => {
+  beforeEach(() => {
+    cy.visit("/");
   });
 
-  it("log the user to de main page", function () {
-    cy.get("input").first().type("pau");
-    cy.get("input").last().type("pau");
-    cy.get("button").click();
+  describe("When the user click on recommended recipe", () => {
+    it("should navigate to detail page", () => {
+      cy.get("li").first().click();
+    });
   });
 });
 
