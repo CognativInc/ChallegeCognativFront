57a761e9e456743078f0e82fa92cc96b769b76e6
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-04-10 12:26:56 +0200

    changed PageContainer responsive padding on Home page

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -11,7 +11,8 @@ const Title = styled.h2`
   margin-bottom: 20px;
 
   @media (max-width: 600px) {
-    margin-left: 10px;
+    margin-left: 15px;
+    margin-bottom: 10px;
     font-size: 18px;
   }
 `;
