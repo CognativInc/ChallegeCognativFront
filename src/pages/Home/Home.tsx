ffacf066d83ab12cc4fc64048a3cfe48faf44a6e
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-04-11 11:43:04 +0200

    added Home styles on styles file

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,8 @@
-import styled from "styled-components";
 import RecipesCardsList from "../../components/RecipesCardsList/RecipesCardsList";
 import Recommended from "../../components/Recommended/Recommended";
 import fakeRecipes from "../../data/fakeRecipes";
 import fakeRecommendations from "../../data/fakeRecommendations";
-
-const PageContainer = styled.div`
-  background-color: #fafafa;
-  padding-top: 50px;
-
-  & > div:nth-child(2n) {
-    gap: 10px;
-    display: flex;
-    flex-direction: column;
-    padding: 20px 100px;
-
-    @media (max-width: 600px) {
-      padding: 10px 15px;
-    }
-  }
-`;
+import { PageContainer } from "./styles";
 
 const Home = (): JSX.Element => {
   return (
