1d68afcccebe8072757a6372b5b7cd8496af6425
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-04-11 11:30:19 +0200

    added SectionTitle styles on styles file

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -1,22 +1,8 @@
-import styled from "styled-components";
-import { recipeDarkGrey } from "../../styles/colors";
+import { Title } from "./styles";
 
 interface TitleProps {
   text: string;
 }
-
-const Title = styled.h2`
-  font-size: 30px;
-  font-weight: bold;
-  color: ${recipeDarkGrey};
-  margin-bottom: 20px;
-
-  @media (max-width: 600px) {
-    margin-left: 15px;
-    margin-bottom: 10px;
-    font-size: 18px;
-  }
-`;
 
 const SectionTitle = ({ text }: TitleProps): JSX.Element => {
   return <Title>{text}</Title>;
